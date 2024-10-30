822409ddec6fd31aaf7fffb895952c04ff7484b9
tusharXsuvera <developer1.suvera@gmail.com>
2024-10-30 11:59:32 +0530

    # Nav Bar improvements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import "../css/Nav.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FaLocationDot } from "react-icons/fa6";
+import { BsCart3 } from "react-icons/bs";
 export default function Nav() {
   const quantity = useSelector((state) => state.cartSlice);
   useEffect(() => {
@@ -10,13 +12,24 @@ export default function Nav() {
   return (
     <div className="nav_bg">
       <div className="split_nav">
-        <Link to="/">
-          <img
-            src="/images/suveraFresh_logo.jpeg"
-            alt="brand logo"
-            className="brand_logo"
-          />
-        </Link>
+        <div className="location_flex">
+          <Link to="/">
+            <img
+              src="/images/suveraFresh_logo.jpeg"
+              alt="brand logo"
+              className="brand_logo"
+            />
+          </Link>
+
+          <div style={{ padding: "5px" }}>
+            <FaLocationDot size={15} />
+          </div>
+          <div>
+            <h1>Kanpur</h1>
+            <h2>Westcott Building</h2>
+          </div>
+        </div>
+
         <Link to="/">
           <h3>Home</h3>
         </Link>
@@ -30,8 +43,8 @@ export default function Nav() {
           <h3>Contact Us</h3>
         </Link>
         <Link to="/" className="split_nav">
-          <h3> ({quantity.value}) </h3>
-          <img src="/images/cart.png" alt="cart image" className="cart_img" />
+          <BsCart3 size={20} />
+          <span className="value"> {quantity.value > 0 && quantity.value}</span>
         </Link>
       </div>
     </div>
