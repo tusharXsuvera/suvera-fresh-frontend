51afd4e7011d2f8cbf32e62ea83ed81b162adf33
tusharXsuvera <developer1.suvera@gmail.com>
2024-10-30 13:19:25 +0530

    # About us improvement with global layout

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -37,29 +37,38 @@ export default function Aboutus() {
         </section>
       </div>
       <div className="mobile_section">
-        <div className="quotes">
-          <h1 className="main_heading">Our Promise</h1>
-          <p className="para">
-            Quality You Can Trust: We source our meats from trusted local farms
-            that prioritize animal welfare and sustainable practices. Freshness
-            Guaranteed: Our products are processed and packaged with care,
-            ensuring you receive only the freshest cuts. Variety for Every
-            Taste: From grass-fed eggs, fish, and organic chicken to specialty
-            meats and seasonal selections, our diverse range caters to every
-            palate.
-          </p>
-        </div>
-        <img src="/images/vision.jpg" alt="mobile screen" className="vision" />
-        <div className="quotes">
-          <h1 className="main_heading">Why Choose Us?</h1>
-          <p className="para">
-            Convenience: No more trips to the grocery store. Enjoy the ease of
-            having fresh meat delivered straight to you. Expert Advice: Our team
-            is here to help! Whether you need cooking tips or recipe ideas,
-            we're just a message away. Satisfaction Guaranteed: If you're not
-            completely satisfied with your order, let us know, and we'll make it
-            right.
-          </p>
+        <div
+          className="mobile_flex global_layout"
+          style={{ paddingTop: "0px", paddingBottom: "0px" }}
+        >
+          <div className="quotes ">
+            <h1 className="main_heading">Our Promise</h1>
+            <p className="para">
+              Quality You Can Trust: We source our meats from trusted local
+              farms that prioritize animal welfare and sustainable practices.
+              Freshness Guaranteed: Our products are processed and packaged with
+              care, ensuring you receive only the freshest cuts. Variety for
+              Every Taste: From grass-fed eggs, fish, and organic chicken to
+              specialty meats and seasonal selections, our diverse range caters
+              to every palate.
+            </p>
+          </div>
+          <img
+            src="/images/vision.jpg"
+            alt="mobile screen"
+            className="vision"
+          />
+          <div className="quotes">
+            <h1 className="main_heading">Why Choose Us?</h1>
+            <p className="para">
+              Convenience: No more trips to the grocery store. Enjoy the ease of
+              having fresh meat delivered straight to you. Expert Advice: Our
+              team is here to help! Whether you need cooking tips or recipe
+              ideas, we're just a message away. Satisfaction Guaranteed: If
+              you're not completely satisfied with your order, let us know, and
+              we'll make it right.
+            </p>
+          </div>
         </div>
       </div>
       <div className="global_layout">
@@ -83,41 +92,43 @@ export default function Aboutus() {
           </div>
           <img src="/images/ceo.jpg" alt="ceo image" className="ceo_image" />
         </section>
+        <section className="ordering_methods">
+          <h1 className="main_heading about-title">
+            FoodChow has introduced three different methods in ordering the
+            food.
+          </h1>
+          <p className="para about-title">
+            Customer of FoodChow can select any method for food ordering.
+            <br />
+            FoodChow is also providing facility to offer all there or any of
+            them ordering method to restaurant owner.
+          </p>
+          <div className="methods_img__flex">
+            <img
+              src="/images/mutton.jpg"
+              alt="methods image"
+              className="methods_img"
+            />
+            <img
+              src="/images/chicken.jpg"
+              alt="methods image"
+              className="methods_img"
+            />
+            <img
+              src="/images/delivery.jpg"
+              alt="methods image"
+              className="methods_img"
+            />
+          </div>
+          <div className="signup_btn__flex">
+            <Link to="/" className="signup_btn__flex signup_btn__about">
+              <span>Sign up in 60 seconds</span>
+              <MdLogin width={100} />
+            </Link>
+          </div>
+        </section>
       </div>
-      <section className="ordering_methods">
-        <h1 className="main_heading about-title">
-          FoodChow has introduced three different methods in ordering the food.
-        </h1>
-        <p className="para about-title">
-          Customer of FoodChow can select any method for food ordering.
-          <br />
-          FoodChow is also providing facility to offer all there or any of them
-          ordering method to restaurant owner.
-        </p>
-        <div className="methods_img__flex">
-          <img
-            src="/images/mutton.jpg"
-            alt="methods image"
-            className="methods_img"
-          />
-          <img
-            src="/images/chicken.jpg"
-            alt="methods image"
-            className="methods_img"
-          />
-          <img
-            src="/images/delivery.jpg"
-            alt="methods image"
-            className="methods_img"
-          />
-        </div>
-        <div className="signup_btn__flex">
-          <Link to="/" className="signup_btn__flex signup_btn__about">
-            <span>Sign up in 60 seconds</span>
-            <MdLogin width={100} />
-          </Link>
-        </div>
-      </section>
+
       <Footer />
     </>
   );
