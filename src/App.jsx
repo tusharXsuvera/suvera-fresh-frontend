f4ef45b771f106df48d499e13f83733e051c6cc7
tusharXsuvera <developer1.suvera@gmail.com>
2024-11-07 18:12:18 +0530

    # code cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import RouteRole from "./RouteRole";
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import ProductDetail from "./pages/ProductDetail";
+import Aboutus from "./pages/Aboutus";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import Cart from "./pages/Cart";
+import Layout from "./components/Layout";
 function App() {
-  return <RouteRole />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="category" element={<Category />} />
+          <Route path="product-detail/:id" element={<ProductDetail />} />
+          <Route path="about-us" element={<Aboutus />} />
+          <Route path="contact-us" element={<Contact />} />
+        </Route>
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
