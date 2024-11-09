01e52f42cdd22b0d40f119064ee3e74d52f82ba8
tusharXsuvera <developer1.suvera@gmail.com>
2024-11-09 16:59:26 +0530

    # code cleaning

--- src/apiCall/api.jsx
+++ src/apiCall/api.jsx
@@ -3,18 +3,16 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const host = `https://suvera-backend.vercel.app/api/`;
 export const handleGetAPI = async (endpoint) => {
-  const url = `${host}${endpoint}`;
   try {
-    const result = await axios.get(url);
+    const result = await axios.get(`${host}${endpoint}`);
     return result.data;
   } catch (err) {
     toast.error(err.message, { autoClose: 1000 });
   }
 };
 export const handlePostAPI = async (endpoint, formdata) => {
-  const url = `${host}${endpoint}`;
   try {
-    const result = await axios.post(url, formdata);
+    const result = await axios.post(`${host}${endpoint}`, formdata);
     return result.data;
   } catch (err) {
     toast.error(err.message, { autoClose: 1000 });
