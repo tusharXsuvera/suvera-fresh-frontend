45d71c683b6a16e445acae887617e0813c3aa6fe
tusharXsuvera <developer1.suvera@gmail.com>
2024-11-14 17:37:08 +0530

    # firebase successfully integrated

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,10 +23,6 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
