97d74666ad5f6c024b8017f1e7779d998280e109
tusharXsuvera <developer1.suvera@gmail.com>
2024-11-15 11:59:53 +0530

    # environment variable set

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,15 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
-import { firebaseConfig } from "../src/utils/somefunc";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLqY53_Mhua0v86ese5Lv_TS1hzMuTBPg",
+  authDomain: "suverafresh-362e4.firebaseapp.com",
+  projectId: "suverafresh-362e4",
+  storageBucket: "suverafresh-362e4.firebasestorage.app",
+  messagingSenderId: "247687775540",
+  appId: "1:247687775540:web:44aea8bba25170fd735161",
+};
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
