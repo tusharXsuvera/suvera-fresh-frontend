378d5cd98eb24eaea60f2258cdf8f387bc0e17cc
devxtushar <devxtushar@gmail.com>
2024-11-29 17:29:05 +0530

    #testing 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,6 @@ export default function Login() {
   const onSubmit = async (data) => {
     let endpoint = "/auth/signin";
     const result = await handlePostAPI(endpoint, data);
-    console.log(result, "");
     if (result && !result.user.verified) {
       navigate("/verify-otp", { state: { phoneNumber: data.phoneNumber } });
     } else if (result && result.token) {
